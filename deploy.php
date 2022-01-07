64ad7ffbb24c43d0cae6f9a89f12270c7b9fd7c9
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-01-07 20:32:02 +0000

    Setup deployer

---
+++ deploy.php
@@ -0,0 +1,27 @@
+<?php
+namespace Deployer;
+
+// https://github.com/deployphp/deployer/blob/master/recipe/common.php
+require 'recipe/common.php';
+
+// Hosts
+host('prod')
+    ->set('remote_user', 'sajad')
+    ->set('deploy_path', '~/sites/wordpress-deployer-example');
+
+// Config
+set('repository', 'git@github.com:sajadtorkamani/wordpress-deployer-example.git');
+
+set('shared_files', ['wp-config.php']);
+set('shared_dirs', ['wp-content/uploads']);
+set('writable_dirs', ['wp-content/uploads']);
+
+
+// Tasks
+desc('Deploy the project');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:publish',
+]);
+
+after('deploy:failed', 'deploy:unlock');
